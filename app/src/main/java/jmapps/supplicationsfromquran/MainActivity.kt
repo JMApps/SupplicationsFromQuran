ac57711c1604e363e4defb95efa812961ff96045
dev_muslim <jmappsmuslim@gmail.com>
2019-10-23 19:23:20 +0300

    Added main ui classes

--- app/src/main/java/jmapps/supplicationsfromquran/MainActivity.kt
+++ app/src/main/java/jmapps/supplicationsfromquran/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import jmapps.supplicationsfromquran.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
---
+++ app/src/main/java/jmapps/supplicationsfromquran/presentation/ui/main/MainViewHolder.kt
@@ -0,0 +1,2 @@
+package jmapps.supplicationsfromquran.presentation.ui.main
+
