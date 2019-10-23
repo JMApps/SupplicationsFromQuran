2c0758fe9bdcb9cc7f43c0adced3b6a47df47c7b
dev_muslim <jmappsmuslim@gmail.com>
2019-10-23 19:07:59 +0300

    Initial commit

---
+++ app/src/main/java/jmapps/supplicationsfromquran/MainActivity.kt
@@ -0,0 +1,28 @@
+package jmapps.supplicationsfromquran
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
