34d5c375ae5357f8629498f68e6c43b18700d048
dev_muslim <jmappsmuslim@gmail.com>
2019-10-24 10:59:00 +0300

    Added splash screen and icon app

---
+++ app/src/main/java/jmapps/supplicationsfromquran/SplashActivity.kt
@@ -0,0 +1,16 @@
+package jmapps.supplicationsfromquran
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+
+class SplashActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val toMainActivity = Intent(this, MainActivity::class.java)
+        startActivity(toMainActivity)
+        finish()
+    }
+}
