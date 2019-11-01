cf39739c5810dae3888e1cfc53051ffb267168f4
dev_muslim <jmappsmuslim@gmail.com>
2019-11-01 08:17:53 +0300

    New database version

--- app/src/main/java/jmapps/supplicationsfromquran/data/database/DatabaseOpenHelper.kt
+++ app/src/main/java/jmapps/supplicationsfromquran/data/database/DatabaseOpenHelper.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper
 import jmapps.supplicationsfromquran.R
 
-private var databaseVersion: Int = 1
+private var databaseVersion: Int = 5
 
 class DatabaseOpenHelper(context: Context?) :
     SQLiteAssetHelper(context, context?.getString(R.string.database_name), null, databaseVersion) {
