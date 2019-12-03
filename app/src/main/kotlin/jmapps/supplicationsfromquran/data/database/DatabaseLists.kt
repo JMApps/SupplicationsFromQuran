0a9eed9969c105fecf0cdaab8e5e6943dad8461d
dev_muslim <jmappsmuslim@gmail.com>
2019-12-03 11:36:31 +0300

    Added bookmark list

--- app/src/main/kotlin/jmapps/supplicationsfromquran/data/database/DatabaseLists.kt
+++ app/src/main/kotlin/jmapps/supplicationsfromquran/data/database/DatabaseLists.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import jmapps.supplicationsfromquran.presentation.ui.bookmarks.BookmarkModel
 import jmapps.supplicationsfromquran.presentation.ui.main.MainModel
 
 class DatabaseLists(private val context: Context?) {
@@ -43,5 +44,40 @@ class DatabaseLists(private val context: Context?) {
                 }
             }
             return contentList
+        }
+
+    val getBookmarkList: MutableList<BookmarkModel>
+        @SuppressLint("Recycle")
+        get() {
+
+            database = DatabaseOpenHelper(context).readableDatabase
+
+            val cursor: Cursor = database.query(
+                "Table_of_contents",
+                null,
+                "Favorite = 1",
+                null,
+                null,
+                null,
+                null
+            )
+
+            val bookmarkList = ArrayList<BookmarkModel>()
+
+            if (cursor.moveToFirst()) {
+                while (!cursor.isAfterLast) {
+                    val bookmarks = BookmarkModel(
+                        cursor.getString(cursor.getColumnIndex("ContentAyah")),
+                        cursor.getString(cursor.getColumnIndex("ContentTranslation")),
+                        cursor.getString(cursor.getColumnIndex("NameAudio")),
+                        cursor.getString(cursor.getColumnIndex("NameDua")))
+                    bookmarkList.add(bookmarks)
+                    cursor.moveToNext()
+                    if (cursor.isClosed) {
+                        cursor.close()
+                    }
+                }
+            }
+            return bookmarkList
         }
 }
